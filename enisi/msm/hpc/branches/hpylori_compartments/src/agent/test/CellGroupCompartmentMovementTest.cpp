8a4926d11074d338e274e09c059f0e7aec9e182e
nathanliles <nathanliles@b376a50f-6cf4-0410-8994-b34eb12c0a4a>
2016-01-26 15:24:17 +0000

    * test in CellGroupCompartmentMovementTest that DEAD state exists in lumen, not epithelium

--- enisi/msm/hpc/branches/hpylori_compartments/src/agent/test/CellGroupCompartmentMovementTest.cpp
+++ enisi/msm/hpc/branches/hpylori_compartments/src/agent/test/CellGroupCompartmentMovementTest.cpp
@@ -6,165 +6,77 @@
 class ACellGroupCompartmentMovement: public ScheduleTestFixture 
 { 
 public:
-  bool _requestCalled, _actCalled, _assertRemoteLocalAgentSyncOccuredCalled;
+  bool _assertTH17inLumenNotEpitheliumCalled, 
+       _moveTH17betweenLumenEpitheliumBarrierCalled, 
+       _assertTH17inEpitheliumNotLumenCalled;
+
   repast::SharedContext<ENISIAgent> * _p_cellContext;
 
   ACellGroupCompartmentMovement() : 
-    _requestCalled(false), _actCalled(false), _assertRemoteLocalAgentSyncOccuredCalled(false),
-    _lumen(_dimensions)
+    _assertTH17inLumenNotEpitheliumCalled(false), 
+    _moveTH17betweenLumenEpitheliumBarrierCalled(false), 
+    _assertTH17inEpitheliumNotLumenCalled(false),
+    _lumen(_dimensions), _epithelium(_dimensions)
     { }
 
-  void SetUp() {
+  void SetUp() 
+  {
     InitSharedContext::SetUp(); 
 
-
-    ENISI::Compartment epithelium(_dimensions);
     ENISI::Compartment gastricLymphNode(_dimensions);
     ENISI::Compartment laminaPropria(_dimensions);
 
     const int tcellCount = 0;
-    tcells = AgentGroupFactory::create("TcellGroup", &_lumen, tcellCount);
+    _lumenTcells = (TcellGroup *) AgentGroupFactory::create(
+    	"TcellGroup", &_lumen, tcellCount);
+    _epitheliumTcells = (TcellGroup *) AgentGroupFactory::create(
+    	"TcellGroup", &_epithelium, tcellCount);
 
     _p_cellContext = _lumen.cellLayer()->context();
-
   }
 
   void TearDown() { }
 
-  void requestAgents();
-  void act();
-  void assertRemoteLocalAgentSyncOccured();
+  void assertTH17inLumenNotEpithelium();
+  void moveTH17betweenLumenEpitheliumBarrier();
+  void assertTH17inEpitheliumNotLumen();
   void assertTransfersCleared();
 
 private:
   ENISI::Compartment _lumen;
+  ENISI::Compartment _epithelium;
 
-  CellGroup * tcells;
+  TcellGroup * _lumenTcells;
+  TcellGroup * _epitheliumTcells;
 };
 
-void ACellGroupCompartmentMovement::requestAgents()
+void ACellGroupCompartmentMovement::assertTH17inLumenNotEpithelium()
 {
-  _requestCalled = true;
-  _lumen.cellLayer()->requestAgents();
+  _assertTH17inLumenNotEpitheliumCalled = true;
+
+  _lumenTcells->addStateAt(TcellGroup::TH17, repast::Point<int>(5,5));
+  TcellGroup::StateCount lumenTcellCount = _lumenTcells->countByState();
+  ASSERT_THAT(lumenTcellCount.state[TcellGroup::TH17], Eq(1));
+
+  TcellGroup::StateCount epitheliumTcellCount = _epitheliumTcells->countByState();
+  ASSERT_THAT(epitheliumTcellCount.state[TcellGroup::TH17], Eq(0));
 }
 
-void ACellGroupCompartmentMovement::act()
+void ACellGroupCompartmentMovement::moveTH17betweenLumenEpitheliumBarrier()
 {
-  _actCalled = true;
-  std::vector<ENISIAgent*> localAgents;
-  _p_cellContext->selectAgents(
-    CellGroup::Context::LOCAL, 
-    localAgents
-  );
-  ASSERT_THAT(localAgents.size(), Eq(1));
-
-  std::vector<ENISIAgent*> remoteAgents;
-  _p_cellContext->selectAgents(
-    CellGroup::Context::NON_LOCAL, 
-    remoteAgents
-  );
-  int worldSize = repast::RepastProcess::instance()->worldSize();
-  int remoteAgentSize = worldSize - 1;
-  ASSERT_THAT(remoteAgents.size(), Eq(remoteAgentSize));
-
-  std::vector<ENISIAgent*> allAgents;
-  _p_cellContext->selectAgents(allAgents);
-  ASSERT_THAT(allAgents.size(), Eq(worldSize));
-
-  std::vector<ENISIAgent*>::const_iterator local_it = localAgents.begin();
-
-  while(local_it != localAgents.end())
-  {
-    TcellGroup * localTcells = (TcellGroup*) (*local_it);
-    localTcells->transferStateTo(TcellGroup::DEAD, repast::Point<int>(0, 0), 5);
-    local_it++;
-  }
-
-  _lumen.cellLayer()->synchronizeAgentStates();
+  _moveTH17betweenLumenEpitheliumBarrierCalled = true;
 }
 
-void ACellGroupCompartmentMovement::assertRemoteLocalAgentSyncOccured()
+void ACellGroupCompartmentMovement::assertTH17inEpitheliumNotLumen()
 {
-  _assertRemoteLocalAgentSyncOccuredCalled = true;
-  std::vector<ENISIAgent*> remoteAgents;
-  _p_cellContext->selectAgents(
-    CellGroup::Context::NON_LOCAL, 
-    remoteAgents
-  );
-
-  std::vector<ENISIAgent*>::const_iterator remote_it = remoteAgents.begin();
-  while(remote_it != remoteAgents.end())
-  {
-    TcellGroup * remoteTcells = (TcellGroup*) (*remote_it);
-
-    TcellGroup::Transfers remoteTransfers = remoteTcells->getTransfers();
-    ASSERT_THAT(remoteTransfers[TcellGroup::DEAD].size(), Eq(5));
-
-    remote_it++;
-  }
-
-  std::vector<ENISIAgent*> localAgents;
-  _p_cellContext->selectAgents(
-    CellGroup::Context::LOCAL, 
-    localAgents
-  );
-
-  std::vector<ENISIAgent*>::const_iterator local_it = localAgents.begin();
-
-  while(local_it != localAgents.end())
-  {
-    TcellGroup * localTcells = (TcellGroup*) (*local_it);
-
-    TcellGroup::Transfers localTransfers = localTcells->getTransfers();
-    ASSERT_THAT(localTransfers[TcellGroup::DEAD].size(), Eq(5));
-
-    localTcells->clearTransfers();
-    local_it++;
-  }
-
-  _lumen.cellLayer()->synchronizeAgentStates();
+  _assertTH17inEpitheliumNotLumenCalled = true;
 }
 
 void ACellGroupCompartmentMovement::assertTransfersCleared()
 {
-  std::vector<ENISIAgent*> remoteAgents;
-  _p_cellContext->selectAgents(
-    CellGroup::Context::NON_LOCAL, 
-    remoteAgents
-  );
-
-  std::vector<ENISIAgent*>::const_iterator remote_it = remoteAgents.begin();
-  while(remote_it != remoteAgents.end())
-  {
-    TcellGroup * remoteTcells = (TcellGroup*) (*remote_it);
-
-    TcellGroup::Transfers remoteTransfers = remoteTcells->getTransfers();
-    ASSERT_THAT(remoteTransfers[TcellGroup::DEAD].size(), Eq(0));
-
-    remote_it++;
-  }
-
-  std::vector<ENISIAgent*> localAgents;
-  _p_cellContext->selectAgents(
-    CellGroup::Context::LOCAL, 
-    localAgents
-  );
-
-  std::vector<ENISIAgent*>::const_iterator local_it = localAgents.begin();
-
-  while(local_it != localAgents.end())
-  {
-    TcellGroup * localTcells = (TcellGroup*) (*local_it);
-
-    TcellGroup::Transfers localTransfers = localTcells->getTransfers();
-    ASSERT_THAT(localTransfers[TcellGroup::DEAD].size(), Eq(0));
-
-    local_it++;
-  }
-
-  ASSERT_THAT(_requestCalled, Eq(true));
-  ASSERT_THAT(_actCalled, Eq(true));
-  ASSERT_THAT(_assertRemoteLocalAgentSyncOccuredCalled, Eq(true));
+  ASSERT_THAT(_assertTH17inLumenNotEpitheliumCalled, Eq(true));
+  ASSERT_THAT(_moveTH17betweenLumenEpitheliumBarrierCalled, Eq(true));
+  ASSERT_THAT(_assertTH17inEpitheliumNotLumenCalled, Eq(true));
 }
 
 TEST_F(ACellGroupCompartmentMovement, SyncsAgentChangesAcrossProcesses)
@@ -176,15 +88,15 @@ TEST_F(ACellGroupCompartmentMovement, SyncsAgentChangesAcrossProcesses)
 
   runner.scheduleEvent(1, repast::Schedule::FunctorPtr(
     new repast::MethodFunctor<ACellGroupCompartmentMovement> (
-      this, &ACellGroupCompartmentMovement::requestAgents)));
+      this, &ACellGroupCompartmentMovement::assertTH17inLumenNotEpithelium)));
 
   runner.scheduleEvent(2, repast::Schedule::FunctorPtr(
     new repast::MethodFunctor<ACellGroupCompartmentMovement> (
-      this, &ACellGroupCompartmentMovement::act)));
+      this, &ACellGroupCompartmentMovement::moveTH17betweenLumenEpitheliumBarrier)));
 
   runner.scheduleEvent(3, repast::Schedule::FunctorPtr(
     new repast::MethodFunctor<ACellGroupCompartmentMovement> (
-      this, &ACellGroupCompartmentMovement::assertRemoteLocalAgentSyncOccured)));
+      this, &ACellGroupCompartmentMovement::assertTH17inEpitheliumNotLumen)));
 
   /*Schedule will repeat infinitely without a stop*/
   runner.scheduleStop(3); 
@@ -193,9 +105,9 @@ TEST_F(ACellGroupCompartmentMovement, SyncsAgentChangesAcrossProcesses)
     new repast::MethodFunctor<ACellGroupCompartmentMovement> (this, 
       &ACellGroupCompartmentMovement::assertTransfersCleared)));
 
-  ASSERT_THAT(_requestCalled, Eq(false));
-  ASSERT_THAT(_actCalled, Eq(false));
-  ASSERT_THAT(_assertRemoteLocalAgentSyncOccuredCalled, Eq(false));
+  ASSERT_THAT(_assertTH17inLumenNotEpitheliumCalled, Eq(false));
+  ASSERT_THAT(_moveTH17betweenLumenEpitheliumBarrierCalled, Eq(false));
+  ASSERT_THAT(_assertTH17inEpitheliumNotLumenCalled, Eq(false));
 
   runner.run();
 
