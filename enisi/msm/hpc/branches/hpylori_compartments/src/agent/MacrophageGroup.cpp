df9464ba7ee6ae075a8ef855ac231601d11d61db
nathanliles <nathanliles@b376a50f-6cf4-0410-8994-b34eb12c0a4a>
2016-02-15 21:32:36 +0000

    * Add skeleton for MacrophageGroup to loop thru coordinates when acting

--- enisi/msm/hpc/branches/hpylori_compartments/src/agent/MacrophageGroup.cpp
+++ enisi/msm/hpc/branches/hpylori_compartments/src/agent/MacrophageGroup.cpp
@@ -32,4 +32,81 @@ MacrophageGroup::MacrophageGroup(
   }
 }
 
-void MacrophageGroup::act() { }
+void MacrophageGroup::act() 
+{ 
+  coordMapConstIter end = coordMapEnd();
+
+  for(coordMapConstIter it = coordMapBegin(); it != end; it++) 
+  {
+    repast::Point<int> loc = it->first;
+    const StateCount count = it->second;
+
+    for (unsigned int i = 0; i < MacrophageState::KEEP_AT_END; ++i)
+    {
+      for (unsigned int j = 0; j < count.state[i]; ++j)
+      {
+	MacrophageState::State state = static_cast<MacrophageState::State>(i);
+	act(state, loc);
+      }
+    }
+  }
+}
+
+void MacrophageGroup::act(
+    MacrophageState::State state, const repast::Point<int> & loc __attribute__((unused)))
+{
+ if (state == MacrophageState::DEAD) return;
+
+  //double IL6  = Cytokines::instance().get("IL6", loc);
+  //double TGFb = Cytokines::instance().get("TGFb", loc);
+  //double IL12 = Cytokines::instance().get("IL12", loc);
+
+  //TcellState::State newState = state;
+
+  //if (IL6 + TGFb + IL12 > 1.0) 
+  //{
+    //[> set initial concentrations <]
+    //TcellODE & odeModel = TcellODE::getInstance();
+    //odeModel.setInitialConcentration("IL6", IL6);
+    //odeModel.setInitialConcentration("TGFb", TGFb);
+    //odeModel.setInitialConcentration("IL12", IL12);
+    
+    //[> run time course <]
+    //odeModel.runTimeCourse();
+    
+    //double IFNg = odeModel.getConcentration("IFNg");
+    //double IL17 = odeModel.getConcentration("IL17");
+    //double IL10 = odeModel.getConcentration("IL10");
+
+//[>    std::cout << "CD4+ T Cell at (" << loc.getX() << ", " << loc.getY() << ")\n";<]
+    ////std::cout << "Inputs Cytokines:\n";
+    ////std::cout << "IL12 : " << IL12 << "\n";
+    ////std::cout << "IL6 : " << IL6 << "\n";
+    ////std::cout << "TGFb : " << TGFb << "\n";
+    ////std::cout << "Outputs Cytokines:\n";
+    ////std::cout << "IFNg : " << IFNg << "\n";
+    ////std::cout << "IL17 : " << IL17 << "\n";
+    //[>std::cout << "IL10 : " << IL10 << "\n";<]
+
+    //[> get output cytokines <]
+    //Cytokines::CytoMap & cytoMap = Cytokines::instance().map();
+    //cytoMap["IFNg"]->setValueAtCoord(IFNg, loc);
+    //cytoMap["IL17"]->setValueAtCoord(IL17, loc);
+    //cytoMap["IL10"]->setValueAtCoord(IL10, loc);
+
+    //if (IL17 > 0.5) {
+      //newState = TcellState::TH17;
+    //} else if (IFNg > 0.5) {
+      //newState = TcellState::TH1;
+    //} else if (IL10 > 0.5) {
+      //newState = TcellState::TREG;
+    //}
+  //}
+
+  //std::vector<double> moveTo = randomMove(1, loc);
+  //repast::Point<int> newLoc(moveTo[0], moveTo[1]);
+
+  //delCellAt(state, loc);
+  /*addCellAt(newState, newLoc);*/
+}
+
