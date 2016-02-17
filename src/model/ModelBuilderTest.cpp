7fca61cb01f4ca87526405fb54adce92db13a15b
nathanliles <nathanliles@b376a50f-6cf4-0410-8994-b34eb12c0a4a>
2016-02-17 18:32:37 +0000

    Wrote equivalent test for cellgroup version of Tcells and Bacteria

--- src/model/ModelBuilderTest.cpp
+++ src/model/ModelBuilderTest.cpp
@@ -27,3 +27,28 @@ TEST_F(AModel, KillsInfectiousBacteriaActingNearTH17)
 
   ASSERT_THAT(p_bacteria->getState(), Eq(AgentState::DEAD));
 }
+
+TEST_F(AModel, KillsInfectiousBacteriaGroupActingNearTH17Group)
+{
+  ENISI::Compartment compartment(_dimensions);
+
+  TcellGroup * p_tcell = 
+    (TcellGroup * )AgentGroupFactory::create("TcellGroup", &compartment);
+  BacteriaGroup * p_bacteria = 
+    (BacteriaGroup * )AgentGroupFactory::create("BacteriaGroup", &compartment);
+
+  repast::Point<int> pt(50, 50);
+
+  p_bacteria->addCellAt(BacteriaState::INFECTIOUS, pt);
+  p_tcell->addCellAt(TcellState::TH17, pt);
+
+  const BacteriaGroup::StateCount * p_countBefore = p_bacteria->getCellsAt(pt);
+  ASSERT_THAT(p_countBefore->state[BacteriaState::INFECTIOUS], Eq(1));
+  ASSERT_THAT(p_countBefore->state[BacteriaState::DEAD], Eq(0));
+
+  p_bacteria->act();
+
+  const BacteriaGroup::StateCount * p_countAfter = p_bacteria->getCellsAt(pt);
+  ASSERT_THAT(p_countAfter->state[BacteriaState::INFECTIOUS], Eq(0));
+  ASSERT_THAT(p_countAfter->state[BacteriaState::DEAD], Eq(1));
+}
