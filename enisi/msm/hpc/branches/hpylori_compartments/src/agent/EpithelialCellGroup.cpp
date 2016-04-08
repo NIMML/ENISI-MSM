6b47e7d2c24de7408443b16d55856437722610e4
meghna <meghna@b376a50f-6cf4-0410-8994-b34eb12c0a4a>
2016-04-08 22:00:42 +0000

    Added rule 12, regarding the IL10 concentration at the border and added parameter IDs for the rest

--- enisi/msm/hpc/branches/hpylori_compartments/src/agent/EpithelialCellGroup.cpp
+++ enisi/msm/hpc/branches/hpylori_compartments/src/agent/EpithelialCellGroup.cpp
@@ -1,8 +1,15 @@
 #include "EpithelialCellGroup.h"
 
 #include "grid/Borders.h"
+#include "DiffuserImpl.h"
 #include "compartment/Compartment.h"
 using namespace ENISI;
+int p_EpiCellDeath;//Rule 11
+int p_EpiProliferation;//Rule 8
+int p_rule10a;
+int p_rule10b;
+int p_rule12;//Rule 12
+
 
 EpithelialCellGroup::EpithelialCellGroup(Compartment * pCompartment, const size_t & count):
   mpCompartment(pCompartment)
@@ -51,7 +58,11 @@ void EpithelialCellGroup::act(const repast::Point<int> & pt)
       mpCompartment->getAgents(pt, 0, -1, Agent::Bacteria, Bacteria);
       concentrations(Agent::Bacteria, Bacteria, BacteriaConcentration);
     }
-
+  else if (mpCompartment->gridBorders()->distanceFromBorder(pt.coords(), Borders::Y, Borders::LOW) < 1.0)
+    {
+      mpCompartment->getAgents(pt, 0, -1, Agent::DiffuserValues,IL10);
+      concentrations(Agent::DiffuserValues, IL10, IL10Concentration);
+    }
 
   for (; it != end; ++it)
     {
@@ -69,7 +80,7 @@ void EpithelialCellGroup::act(const repast::Point<int> & pt)
       double th17Concentration = TcellsCellConcentration[TcellState::TH17]; //Rule 10 when Th17 is in contact
       double th1Concentration = TcellsCellConcentration[TcellState::TH1]; //RUle 9 when Th1 is in contact
 
-      if (infectiousBacteriaConcentration && state == EpithelialCellState::HEALTHY)
+      if (infectiousBacteriaConcentration && state == EpithelialCellState::HEALTHY && (p_rule10a > repast::Random::instance()->createUniDoubleGenerator(0.0, 1.0).next()))
         {
           newState = EpithelialCellState::DAMAGED;
         }
@@ -77,27 +88,35 @@ void EpithelialCellGroup::act(const repast::Point<int> & pt)
         {
           newState = EpithelialCellState::HEALTHY;
         }
-      else if (th17Concentration && state == EpithelialCellState::HEALTHY
-               && mpCompartment->getType() == Compartment::lamina_propria) // TODO CRITICAL This will never be true
+      else if ((th17Concentration || th1Concentration) && state == EpithelialCellState::HEALTHY
+               && mpCompartment->getType() == Compartment::epithilium && (p_rule10b > repast::Random::instance()->createUniDoubleGenerator(0.0, 1.0).next())) // TODO CRITICAL This will never be true-FIXED
         {
           newState = EpithelialCellState::DAMAGED; /*Rule 10*/
           /* CHECK : Here there should be a function for information regarding the Layer,
           for eg. This rule requires the state transition when TH17 in LaminaPropria is in contact with E at 'Epithelium and LaminaPropria' membrane*/
         }
-      else if (th1Concentration && state == EpithelialCellState::HEALTHY
-               && mpCompartment->getType() == Compartment::lamina_propria) // TODO CRITICAL This will never be true
-        {
-          newState = EpithelialCellState::DAMAGED; /*Rule 9*/
-          /* CHECK : Here there should be a function for information regarding the Layer,
-          for eg. This rule requires the state transition when TH1 in LaminaPropria is in contact with E at 'Epithelium and LaminaPropria' membrane*/
-        }
-      else if (state == EpithelialCellState::HEALTHY &&
-          mpCompartment->getType() == Compartment::epithilium) // TODO CRITICAL This will always be true
-        {
-          // addCellAt(state, newLoc);/* Rule 8*/
-          mpCompartment->removeAgent(pAgent); /*Rule 11*/
-        }
+      if (p_EpiCellDeath > repast::Random::instance()->createUniDoubleGenerator(0.0, 1.0).next())
+              {
+                mpCompartment->removeAgent(pAgent);/*Rule 11*/
+                continue;
+              }
+      if (mpCompartment->getType() == Compartment::epithilium && (p_EpiProliferation > repast::Random::instance()->createUniDoubleGenerator(0.0, 1.0).next()))
+             {
+               mpCompartment->getLocation(pAgent->getId(), Location);/*Rule 8*/
+               mpCompartment->addAgent(new Agent(Agent::EpithelialCell, pAgent->getState()), Location);
+             }
+      else if (mpCompartment->getType() == Compartment::epithilium && state == EpithelialCellState::DAMAGED
+    		  && IL10Concentration > ENISI::Threshold && (p_rule12 > repast::Random::instance()->createUniDoubleGenerator(0.0, 1.0).next()))
+      {
+    	  newState = EpithelialCellState::HEALTHY;/* If IL10 is in contact with E at the Ep and Lm border, E-> Edamaged slowed donw by some factor*/
+      }
 
+      //else if (mpCompartment->getType() == Compartment::epithilium) // TODO CRITICAL This will always be true -FIXED
+        //{
+          // addCellAt(state, newLoc);/* Rule 8*/
+          //mpCompartment->removeAgent(pAgent); /*Rule 11*/
+        //}
+      //else if
       if (newState == EpithelialCellState::DAMAGED)
         {
           mpCompartment->cytokineValue("IL6", pt) = 70;
