14802af6dde76a12ee61e721e4ef05b4bbcd18ed
shoops <shoops@b376a50f-6cf4-0410-8994-b34eb12c0a4a>
2016-05-24 14:46:49 +0000

    Created ENISI Eclipse Workspace

--- enisi/msm/hpc/branches/hpylori_compartments/run.sh
+++ enisi/msm/hpc/branches/hpylori_compartments/run.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-ARGS="$@"
 DIR="$(dirname $0)"
+CONFIG=$1
+
+# Uncomment if you want to run Valgrind
+VALGRIND="/usr/bin/valgrind --track-origins=yes --log-file=memcheck.out.$$ --suppressions=/home/shoops/.memcheck.suppression"
+
+# Clear output and log files
+(cd "$CONFIG"; rm *.tsv *.log)
+
 cd "$DIR"
-"$DIR"/deps/src/project_repast/ext/MPICH/bin/mpirun -n 9 \
-  -host localhost "$DIR"/bin/main $ARGS
+
+"$DIR"/deps/src/project_repast/ext/MPICH/bin/mpirun -n 24 -host localhost \
+   ${VALGRIND} "${DIR}"/bin/main config=${CONFIG}
+
