21aa61397a2e3c38ec94a2467210faf62205c61e
Sook Shin Ha <SookHa@users.noreply.github.com>
2016-05-25 17:04:58 +0000

    Removed obsolete files.

--- src/compartment/CellLayer.cpp
+++
@@ -1,7 +0,0 @@
-#include "compartment/CellLayer.h"
-
-using namespace ENISI;
-
-/* CellLayer class */
-CellLayer::CellLayer(const repast::GridDimensions & dims)
-  : ICompartmentLayer<Agent, AgentGroupPackage, AgentGroupPackageProvider, AgentGroupPackageReceiver>("", dims, dims) { }
