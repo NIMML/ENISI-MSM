719add5044b72828bc044ff28632aa1841b34de3
herbst <herbst@b376a50f-6cf4-0410-8994-b34eb12c0a4a>
2016-09-28 20:24:27 +0000

    fixed broken links to googlemock

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ DIR="$(dirname $0)"
 CONFIG=$1
 
 # Uncomment if you want to run Valgrind
-#VALGRIND="/usr/bin/valgrind --track-origins=yes --log-file=memcheck.out.$$ --suppressions=/home/meghna89/.memcheck.suppression"
+#VALGRIND="/usr/bin/valgrind --track-origins=yes --log-file=memcheck.out.$$ --suppressions=/home/aherbst/.memcheck.suppression"
 
 # Clear output and log files
 (cd "$CONFIG"; rm *.tsv *.log)
