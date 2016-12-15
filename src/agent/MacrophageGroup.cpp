30dd54b088ab2613014a9415b80f54efb985de41
meghna-verma <meghna89@vt.edu>
2016-12-15 12:06:47 -0500

    Natu. death of  Mregs&Minf cond indepen.
    Natural death of Mregs and Minfs are condition independent

--- src/agent/MacrophageGroup.cpp
+++ src/agent/MacrophageGroup.cpp
@@ -178,8 +178,7 @@ void MacrophageGroup::act(const repast::Point<int> & pt)
 
           mpCompartment->cytokineValue("eIL10", pt) += 7;
 
-          if ((macrophageregConcentration > ENISI::Threshold)
-              && (p_rule28a > repast::Random::instance()->createUniDoubleGenerator(0.0, 1.0).next())){
+          if (p_rule28a > repast::Random::instance()->createUniDoubleGenerator(0.0, 1.0).next()){
               LocalFile::debug() << "*** Macrophage_Regulatory dies naturally" << std::endl;
               mpCompartment->removeAgent(pAgent);
               continue;
@@ -189,8 +188,7 @@ void MacrophageGroup::act(const repast::Point<int> & pt)
      if (state == MacrophageState::INFLAMMATORY){
           mpCompartment->cytokineValue("eIFNg", pt) += 7;
 
-          if ((macrophageinfConcentration > ENISI::Threshold)
-              || (p_rule28b > repast::Random::instance()->createUniDoubleGenerator(0.0, 1.0).next())){
+          if (p_rule28b > repast::Random::instance()->createUniDoubleGenerator(0.0, 1.0).next()){
               LocalFile::debug() << "*** Macrophage_Inflamatory dies naturally" << std::endl;
               mpCompartment->removeAgent(pAgent);
               continue;
