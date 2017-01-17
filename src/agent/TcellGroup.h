1f32cbeb7c54c05738151c71df6094e553dadeb9
meghna-verma <meghna89@vt.edu>
2017-01-17 18:31:44 -0500

    Updated the errors in file

--- src/agent/TcellGroup.h
+++ src/agent/TcellGroup.h
@@ -25,34 +25,34 @@ protected:
   virtual void write(const repast::Point<int> & pt);
 
 private:
-  pModel->getValue("p_Th17death", p_Th17death);
-  pModel->getValue("p_naiveTcelldeath",p_naiveTcelldeath);
-  pModel->getValue("p_iTregdeath", p_iTregdeath);
-  pModel->getValue("p_Th1death7", p_Th1death);
-  pModel->getValue("p_nTtoTr", p_nTtoTr);
-  pModel->getValue("p_nTtoTh1 ", p_nTtoTh1);
-  pModel->getValue("p_nTtoiTreg", p_nTtoiTreg);
-  pModel->getValue("p_nTtoTh17",p_nTtoTh17);
-  pModel->getValue("p_nTtoTh1", p_nTtoTh1);
-  pModel->getValue("p_rule31a",p_rule31a);
-  pModel->getValue("p_Th17toiTregs", p_Th17toiTregs);
-  pModel->getValue("p_Th17toiTreg", p_Th17toiTreg);
-  pModel->getValue("p_iTregtoTh17s", p_iTregtoTh17s);
-  pModel->getValue("p_iTregtoTh17", p_iTregtoTh17);
-  pModel->getValue("p_Th17move", p_Th17move);
-  pModel->getValue("p_iTregmove", p_iTregmove);
-  pModel->getValue("p_iTh1move", p_iTh1move);
-  pModel->getValue("p_allTpro", p_allTpro); 
-  pModel->getValue("p_TotalTcap",p_TotalTcap);  
-  pModel->getValue("p_nTcap",p_nTcap);
-  pModel->getValue("p_Th1cap",p_Th1cap);
-  pModel->getValue("p_Th17cap",p_Th17cap);
-  pModel->getValue("p_iTregcap",p_iTregcap);
-  pModel->getValue("p_Trcap",p_Trcap);	  
-  pModel->getValue("p_Th1cyto",p_Th1cyto);	
-  pModel->getValue("p_Th17cyto",p_Th17cyto);
-  pModel->getValue("p_iTregcyto",p_iTregcyto);
-  pModel->getValue("p_Trcyto",p_Trcyto);	
+  double  p_Th17death;
+  double  p_naiveTcelldeath;
+  double  p_iTregdeath;
+  double  p_Th1death7;
+  double  p_nTtoTr;
+  double  p_nTtoTh1 ;
+  double  p_nTtoiTreg;
+  double  p_nTtoTh17;
+  double  p_nTtoTh1;
+  double  p_rule31a;
+  double  p_Th17toiTregs;
+  double  p_Th17toiTreg;
+  double  p_iTregtoTh17s;
+  double  p_iTregtoTh17;
+  double  p_Th17move;
+  double  p_iTregmove;
+  double  p_iTh1move;
+  double  p_allTpro;
+  double  p_TotalTcap  ;
+  double  p_nTcap;
+  double  p_Th1cap;
+  double  p_Th17cap;
+  double  p_iTregcap;
+  double  p_Trcap;
+  double  p_Th1cyto;	
+  double  p_Th17cyto;
+  double  p_iTregcyto;
+  double  p_Trcyto;	
   //double p_rule56;/*Rule 56 parameter*/
   double neardistane_border;/* For the rules that mention nearest distance from Border, earlier it was set as 2*/
 
