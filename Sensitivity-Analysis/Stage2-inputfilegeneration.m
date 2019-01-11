97d63875e5e7c51f66d1f6809373cf03c31ae68b
Meghna Verma <meghna89@vt.edu>
2019-01-11 17:25:19 -0500

    Update Stage2-inputfilegeneration.m

--- Sensitivity-Analysis/Stage2-inputfilegeneration.m
+++ Sensitivity-Analysis/Stage2-inputfilegeneration.m
@@ -1,16 +1,9 @@
-%Author: Meghna Verma
-%Date: June 19, 2017
+% Author: Meghna Verma
+% Last Updated date: June 19, 2017
 % This file is used to create cell arrays using ENISI output files
+% Keep this in the same folder as where the outputs from the hybrid
+% computer model are.
 
-%The code mainly uses the csv files from the csvs output folders in each
-%setting folder inside ENISI outputs and assisgns that as 1 element in the
-%cell array.
-
-%Cellarray_template = cell(180,10,152); % cell array will have 10 columns 
-%representing the 10 replicates and 152 cells show the different sets
-
-%sample1 = csv2cell('output.csv', 'fromfile'); 
-% The function converts csv file to cell array 
 files = dir('*.csv');
 output_LP_HP = {};
 
